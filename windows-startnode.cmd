b08559ea1dd0ed84c900202f5465c891da212c36
Sven-Uhlig <sven.uhlig@gmx.net>
2020-01-22 19:52:51 +0100

    first commit

---
+++ windows-startnode.cmd
@@ -0,0 +1,1 @@
+geth --networkid 4224 --mine --minerthreads 2 --datadir "." --nodiscover --rpc --rpcport "8545" --port "30303" --rpccorsdomain "*" --nat "any" --rpcapi eth,web3,personal,net --unlock e4430e93a9151b5b9a76e181cd10a7e70abd318a --password ./password.sec --allow-insecure-unlock
